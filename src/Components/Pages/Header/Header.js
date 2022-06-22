2caff9fb5b9bebeebba7146d9c0b5f6a91f5bfdc
Karima-Aktari <karimaaktari911@gmail.com>
2022-06-22 21:34:12 +0600

    images position

--- src/Components/Pages/Header/Header.js
+++ src/Components/Pages/Header/Header.js
@@ -12,15 +12,15 @@ import image5 from '../../../../src/Images/1.svg';
 
 const Header = () => {
     return (
-        <div className='banner py-4'>
+        <div className='banner py-4 mx-auto'>
             <div className="heading flex justify-around">
                 <h1 className="text-white">C0SMOS</h1>
                 <div className="header">
                     <nav>
                         <NavLink to="/home">Home</NavLink>
 
-                        <NavLink to="/department">Department</NavLink>
-                        <NavLink to="/specialist">Specialist</NavLink>
+                        <NavLink to="/home">Build</NavLink>
+                        <NavLink to="/home">Explore</NavLink>
                         {/* <button data-tooltip-target="tooltip-bottom" data-tooltip-placement="bottom" type="button" class="mb-2 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Tooltip bottom</button>
                             <div id="tooltip-bottom" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                                 Tooltip on bottom
@@ -41,23 +41,28 @@ const Header = () => {
                 <h4 className='text-gray-100 text-6xl text-center'>Blockchains.</h4>
             </div>
             <section>
-                <div className='flex justify-end p-4'>
+                <div className='flex justify-end p-4 -mt-9 click-to-top avatar' data-tooltip="Thinking Cat">
+
+                    {/* <Tooltip title="tooltip text">
+<img src={image} alt=""/>*/}
                     <img src={image} alt="" />
+                    <span>Tooltip text</span>
                 </div>
-                <div className='flex justify-start p-4'>
+                <div className='flex justify-start p-4 -mt-16 pl-10'>
                     <img src={image1} alt="" />
                 </div>
                 <div className='flex justify-around p-4'>
                     <img src={image2} alt="" />
-                    <img src={image3} alt="" />
+                    <div className='-mt-10' ><img src={image3} alt="" /></div>
                 </div>
 
-                <div className='flex justify-start p-4'>
+                <div className='flex justify-between p-4 px-12'>
                     <img src={image4} alt="" />
+                    <div className='flex justify-end p-4 -mb-10'>
+                        <img src={image5} alt="" />
+                    </div>
                 </div>
-                <div className='flex justify-end p-4'>
-                    <img src={image5} alt="" />
-                </div>
+
 
             </section>
 
